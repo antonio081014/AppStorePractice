c3875c5425b1fb1f952ec7455ff47ba79d82437e
Antonio081014 <antonio081014@antonio081014.com>
2017-08-17 23:52:45 -0700

    Add NameLabel and BUY Button.

--- AppStore/AppDetailCollectionViewController.swift
+++ AppStore/AppDetailCollectionViewController.swift
@@ -46,6 +46,7 @@ class AppDetailHeader: BaseCell {
             if let imageName = app?.imageName {
                 self.imageView.image = UIImage(named: imageName)
             }
+            self.nameLabel.text = app?.name
         }
     }
     
@@ -61,7 +62,28 @@ class AppDetailHeader: BaseCell {
     let segmentedControl: UISegmentedControl = {
         let sc = UISegmentedControl(items: ["Details", "Reviews", "Related"])
         sc.translatesAutoresizingMaskIntoConstraints = false
+        sc.tintColor = .darkGray
+        sc.selectedSegmentIndex = 0
         return sc
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Test"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let buyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("BUY", for: .normal)
+        button.layer.borderColor = UIColor(red: 0, green: 129/255, blue: 250/255, alpha: 1).cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        return button
     }()
     
     override func setupViews() {
@@ -69,9 +91,18 @@ class AppDetailHeader: BaseCell {
         
         self.addSubview(self.imageView)
         self.addSubview(self.segmentedControl)
+        self.addSubview(self.nameLabel)
+        self.addSubview(self.buyButton)
         
-        self.addConstraint(with: "H:|-14-[v0(100)]", views: self.imageView)
+        self.addConstraint(with: "H:|-14-[v0(100)]-8-[v1]|", views: self.imageView, self.nameLabel)
         self.addConstraint(with: "V:|-14-[v0(100)]", views: self.imageView)
+        
+        self.addConstraint(with: "V:|-14-[v0(20)]", views: self.nameLabel)
+        
+        self.addConstraint(with: "H:|-40-[v0]-40-|", views: self.segmentedControl)
+        self.addConstraint(with: "V:[v0(32)]-8-[v1(34)]-8-|", views:self.buyButton, self.segmentedControl)
+        
+        self.addConstraint(with: "H:[v0(60)]-16-|", views: self.buyButton)
     }
 }
 
